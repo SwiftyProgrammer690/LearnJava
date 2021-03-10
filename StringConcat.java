5cd4ce2536cf15aaff144277af2d3404c8ad66f9
Swarit Choudhari <68472469+SwiftyProgrammer690@users.noreply.github.com>
2021-03-09 21:36:02 -0500

    Create StringConcat.java

---
+++ StringConcat.java
@@ -0,0 +1,16 @@
+public class StringConcat {
+
+	public static void main(String[] args) {
+		String s1 = "Hello, World!";
+		System.out.println(s1);
+		
+		String s2 = "1000";
+		System.out.println(s2);
+		
+		String hello = "Hello";
+		String java = "Java!";
+		System.out.println(hello+java);
+		
+	}
+
+}
